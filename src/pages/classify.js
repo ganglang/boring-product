9e576ebc87e1ec60c55ce7966666e0ebdfe32d65
ganglang <1494796010@qq.com>
2019-11-05 17:53:27 +0800

    init

---
+++ src/pages/classify.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+class Classify extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                选择
+            </div>
+        )
+    }
+}
+
+export default Classify;
---
+++ src/pages/personal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Personal extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return ( 
+            <div>
+                personal 
+            </div>
+        )
+    }
+}
+
+export default Personal;
